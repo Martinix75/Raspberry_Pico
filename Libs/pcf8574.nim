a4fb32c58f15bb9809c0a7a733ada59848af08dd
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2021-10-12 14:12:12 +0200

    Add files via upload
    rename a procs! more coerent

--- Libs/pcf8574.nim
+++ Libs/pcf8574.nim
@@ -20,25 +20,25 @@ type
     blockk: I2cInst
     buffer: uint8
 
-proc writeBytex*(self: Pcf8574, dato: uint8 ) = #proc to write the byte 
+proc writeByte*(self: Pcf8574, dato: uint8 ) = #proc to write the byte 
   let dato = dato.unsafeAddr #get the address of the data
   writeBlocking(self.blockk, self.addressDevice, dato, 1, true) #write the data on the i2c bus 
 
-proc readBytex*(self: Pcf8574, dato: var array[1,uint8]) =
+proc readByte*(self: Pcf8574, dato: var array[1,uint8]) =
   let datox = dato[0].unsafeAddr
   discard readBlocking(self.blockk, self.addressDevice, datox, 1, false)
 
-proc digitWrite*(self: Pcf8574, pin: uint8, value: bool) =
+proc writeBit*(self: Pcf8574, pin: uint8, value: bool) =
   if value == on:
     self.buffer = (self.buffer or pin) #go to act (turn on) the selected bit 
-    self.writeBytex(self.buffer)
+    self.writeByte(self.buffer)
   elif value == off:
     self.buffer = (self.buffer xor pin) #go to act (turn off) the selected bit 
-    self.writeBytex(self.buffer)
+    self.writeByte(self.buffer)
 
-proc digitRead*(self: Pcf8574, pin: uint8): bool =
+proc readBit*(self: Pcf8574, pin: uint8): bool =
   var buff = [uint8(0)]
-  readBytex(self, buff)
+  readByte(self, buff)
   let valuePins = buff[0]
   let mask = (valuePins xor 0b11111111)
   let valuePin = (mask and pin)
@@ -48,15 +48,15 @@ proc digitRead*(self: Pcf8574, pin: uint8): bool =
   else:
     result =  off
 
-proc setLow*(self: Pcf8574) = #set buffer 0x00
-  self.writeBytex(0x00)
+proc setLow*(self: Pcf8574) = #set buffer 0x00 all 0
+  self.writeByte(0x00)
 
-proc setHigh*(self: Pcf8574) = #set buffer 0xff
-  self.writeBytex(0xff)
+proc setHigh*(self: Pcf8574) = #set buffer 0xff all 1
+  self.writeByte(0xff)
 
 when isMainModule:
   stdioInitAll()
-  let exp1 = Pcf8574(addressDevice: 0x20, blockk: i2c1)#, buffer: 0x00)# initializes the object if if necessary
+  let exp1 = Pcf8574(addressDevice: 0x20, blockk: i2c1)#, buffer: 0x00 initializes the object if if necessary
 
   const sda = 2.Gpio 
   const scl = 3.Gpio 
@@ -64,36 +64,33 @@ when isMainModule:
   init(i2c1,10000)
   sda.setFunction(I2C); sda.pullUp()
   scl.setFunction(I2C); scl.pullUp()
-
-  #var buffer:uint8 = 0b00000000
-  #writeBytex(expander, buffer)
   sleep(1000)
 
   while true:
     exp1.setHigh() #set all led on
     sleep(1000)
-    exp1.digitWrite(p1, on) #turn on the bit "p1" 
+    exp1.writeBit(p1, on) #turn on the bit "p1" 
     sleep(800)
-    exp1.digitWrite(p4, on) #turn on the bit "p4" 
+    exp1.writeBit(p4, on) #turn on the bit "p4" 
     sleep(1000)
-    exp1.digitWrite(p1, off) #turn off the bit "p1" 
+    exp1.writeBit(p1, off) #turn off the bit "p1" 
     sleep(1000)
-    exp1.digitWrite(p4, off) #turn off the bit "p4" ]#
+    exp1.writeBit(p4, off) #turn off the bit "p4" ]#
     sleep(500)
-    #exp1.writeBytex(0xaa) #alternating leds 
+    #exp1.writeByte(0xaa) #alternating leds 
     #sleep(1000)
     #exp1.setLow() #set all led off
     #sleep(1000)
     #var lettura = [uint8(0)] #create a variable uint8 = 0
-    #exp1.readBytex(lettura) #read all byte on device 
+    #exp1.readByte(lettura) #read all byte on device 
     #print("Leggo sul expander: " & $lettura[0]) #stampa tutto il byte
     #[var p3val = exp1.digitRead(p3) #assigns p3val only the value of bit p3 (bool) 
     print("P3= " & $p3val) #prints the value of bit p3
     sleep(200)
     if p3val == true:
-      exp1.digitWrite(p0, on)
+      exp1.writeBit(p0, on)
     else:
-      exp1.digitWrite(p0, off)]#
+      exp1writeBit(p0, off)]#
 
     #sleep(1900)
     
